1db9608cc4cb1a7b1604e2e04793056b911efdd7
DaSerialGenius <DaSerialGenius@gmail.com>
2020-08-07 22:46:53 +0530

    Listings are now linked to the database
    Books and Authors now show up from the database. If the database is corrupted for some reason, go to  /home to re-enter database values

--- app.py
+++ app.py
@@ -18,62 +18,53 @@ class reg(db.Model):
     password = db.Column(db.String(100), nullable=False)
 
 
-class Book(db.Model):
+def convert_to_int(value):
+        try:
+                return int(value)
+        except:
+                return -1
+
+class Listing(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         name = db.Column(db.String(200), nullable=False)
         summary = db.Column(db.String(256), nullable=True)
-        coversrc = db.Column(db.String(256))
+        author = db.Column(db.String(256), nullable=True)
+        likes = db.Column(db.Integer, nullable = False)
+        date_published= db.Column(db.DateTime)
+        related = db.Column(db.String(256), nullable=True)
+        tags = db.Column(db.String(512), nullable=True)
+        is_author = db.Column(db.Integer, nullable = False)
+
+        def get_author(self):
+                int_list = self.author.split(' ')
+                toCheck = []
+                for i in range(len(int_list)):
+                        toCheck.append(convert_to_int(int_list[i]))
+                toRet=[]
+                for i in toCheck:
+                        toRet.append(Listing.query.get(i))
+                return toRet
 
         def __repr__(self):
-                return '<Task %r>' % self.id
-
-class Author():
-        name = "Peter Kropotkin"
-
-class TB2:
-        id = 1
-        name = "Das Kapital"
-        summary = "Das Kapital, also called Capital. A Critique of Political Economy, is a foundational theoretical text in materialist philosophy, economics and politics by Karl Marx."
-        coversrc = os.path.join(app.config['IMAGE_FOLDER'], name + '.png')
-
-class TB3:
-        id = 3
-        name = "Antifa The Antifascist Handbook"
-        summary = "Antifa: The Anti-Fascist Handbook is a 2017 book by historian Mark Bray, which explores the history of anti-fascist movements since the 1920s and 1930s and their contemporary resurgence."
-        coversrc = os.path.join(app.config['IMAGE_FOLDER'], name + '.png')
-
-class TB1:
-        id = 0
-        likes = "100%"
-        name = "The Conquest of Bread"
-        summary = "The Conquest of Bread is an 1892 book by the Russian anarcho-communist Peter Kropotkin. Originally written in French, it first appeared as a series of articles in the anarchist journal Le Révolté. It was first published in Paris with a preface by Élisée Reclus, who also suggested the title."
-        coversrc = os.path.join(app.config['IMAGE_FOLDER'], name + '.png')
-        trivia=["I am a human", "Shahi paneer is tasty", "water is wet", "oh wait I was supposed to write about the book", "oh shit oh fu-"]
-        author=[Author()]
-        date=datetime.now().date()
-        related = [TB2(), TB3()]
+                return '<Listing %r>' % self.id
 
 
-
-
-@app.route('/information', methods=['POST', 'GET'])
-def index():
-        book = TB1()
-        return render_template("information.html", listing=book)
+@app.route('/information/<int:id>', methods=['POST', 'GET'])
+def index(id):
+        l = Listing.query.get(id)
+        return render_template("information.html", listing=l)
 
 
 @app.route('/register',methods=['POST', 'GET'])
 def about():
         if request.method == 'POST':
-
                 username = request.form['username']
                 email = request.form['email']
                 password = request.form['password']
                 new_username = reg(username=username,email=email,password=password)
                 
                 try:
-                        db.session.add(new_username)
-                        
+                        db.session.add(new_username)                
                         db.session.commit()
                         return redirect('/login')
                 except:
@@ -81,13 +72,20 @@ def about():
 
         else:
                 new_username = reg.query.order_by(reg.username).first();   
-                #new_username = 0
                 return render_template('register.html',new_username=new_username)
         
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+        book = Listing(id=0, name="The Conquest of Bread",summary="Fluffy boi", author="1", likes=1, date_published=datetime.now().date(),related="", tags="", is_author=False)
+        kropotkin = Listing(id=1, name="Peter Kropotkin",summary="Leftist Santa", author="0 ", likes=69, date_published=datetime.now().date(),related="", tags="", is_author=True)
+        
+        try:
+                db.session.add(kropotkin)
+                db.session.commit()
+                return redirect("information/0")
+        except:
+                return "SOMETHING WENT WRONG, PLEASE TRY AGAIN"
 
 
 @app.route('/login',methods=['POST', 'GET'])
